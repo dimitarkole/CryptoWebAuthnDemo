cf702fc07edbb261c7beaab7a7510ed90c9be87d
Димитър Колев <dim_kolev2002@abv.bg>
2026-02-19 21:08:58 +0200

    WebAuthnService

---
+++ CryptoWebAuthManager/Data/CryptoWebAuthnManager.Data/Migrations/20260219183630_Initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWebAuthnManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebAuthnCredentials",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CredentialId = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PublicKey = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    SignatureCounter = table.Column<long>(type: "bigint", nullable: false),
+                    CredType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebAuthnCredentials", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebAuthnCredentials_IsDeleted",
+                table: "WebAuthnCredentials",
+                column: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebAuthnCredentials");
+        }
+    }
+}
---
+++ CryptoWebAuthManager/Services/CryptoWebAuthnManager.Services.Data/WebAuthnService.cs
@@ -0,0 +1,97 @@
+﻿namespace CryptoWebAuthnManager.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using CryptoWebAuthnManager.Data.Common.Repositories;
+    using CryptoWebAuthnManager.Data.Models;
+    using Fido2NetLib;
+    using Fido2NetLib.Objects;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Options;
+
+    public class WebAuthnService : IWebAuthnService
+    {
+        private readonly Fido2 _fido2;
+        private readonly IRepository<WebAuthnCredential> credentialsRepo;
+
+        public WebAuthnService(
+            IConfiguration config,
+            IRepository<WebAuthnCredential> credentialsRepo)
+        {
+            var fidoConfig = new Fido2Configuration
+            {
+                ServerDomain = config["Fido:ServerDomain"],
+                ServerName = config["Fido:ServerName"],
+                Origins = new HashSet<string> { config["Fido:Origin"] }
+            };
+
+            this._fido2 = new Fido2(fidoConfig);
+            this.credentialsRepo = credentialsRepo;
+        }
+
+        public async Task<CredentialCreateOptions> GenerateRegistrationOptionsAsync(string userId, string username)
+        {
+            var user = new Fido2User
+            {
+                DisplayName = username,
+                Name = username,
+                Id = Encoding.UTF8.GetBytes(userId)
+            };
+
+            var requestNewCredentialParams = new RequestNewCredentialParams()
+            {
+                User = user,
+            };
+
+            return _fido2.RequestNewCredential(requestNewCredentialParams);
+        }
+
+        public async Task<bool> CompleteRegistrationAsync(
+            string userId,
+            AuthenticatorAttestationRawResponse attestationResponse,
+            CredentialCreateOptions originalOptions)
+        {
+            // Callback за уникалност
+            async Task<bool> Callback(IsCredentialIdUniqueToUserParams args, CancellationToken ct)
+            {
+                var exists = credentialsRepo
+                    .All()
+                    .Any(c => c.CredentialId.SequenceEqual(args.CredentialId));
+
+                return !exists;
+            }
+
+            var success = await _fido2.MakeNewCredentialAsync(
+                new MakeNewCredentialParams()
+                {
+                    AttestationResponse = attestationResponse,
+                    OriginalOptions = originalOptions,
+                    IsCredentialIdUniqueToUserCallback = Callback,
+                },
+                CancellationToken.None);
+
+            var credential = new WebAuthnCredential
+            {
+                UserId = userId,
+                CredentialId = success.Id,
+                PublicKey = success.PublicKey,
+                SignatureCounter = success.SignCount,
+                CredType = success.Type.ToString(),
+                CreatedOn = DateTime.UtcNow
+            };
+
+            await credentialsRepo.AddAsync(credential);
+            await credentialsRepo.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
